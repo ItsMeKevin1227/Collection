4888c6f5ad3bd0e8458caec33f8cae12e86b18b7
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2018-07-14 11:20:49 +0800

    增加一些驗證功能(未全部完成)

--- 06_F2E_Validation/validate/validate.js
+++ 06_F2E_Validation/validate/validate.js
@@ -1,37 +1,31 @@
 import Vue from 'vue';
 import VeeValidate, { Validator } from 'vee-validate';
-import zh from 'vee-validate/dist/locale/zh_TW'; // 引入中文文件
-
-// 配置中文
-Validator.addLocale(zh);
-
-const config = {
-  locale: 'zh_TW',
-};
-
-Vue.use(VeeValidate, config);
 
 // 自定義validate
 const dictionary = {
-  zh_CN: {
+  en: {
     messages: {
-      email: () => '請輸入正確的電子郵件格式',
-      required: field => `請輸入${field}`,
+      email: () => 'INVALID EMAIL',
+      required: field => `${field} CAN NOT BE EMPTY`,
+      min: () => 'MINIMUM 8 CHARACTERS',
     },
     attributes: {
-      email: '電子郵件',
-      password: '密碼',
-      name: '帳號',
-      phone: '手機',
+      email: 'Account',
+      password: 'Password',
+      confirmPsw: 'Confirm Password',
+      phone: 'Phone',
+      name: 'Name',
     },
   },
 };
 
-Validator.updateDictionary(dictionary);
+Validator.localize(dictionary);
 
 Validator.extend('phone', {
   messages: {
-    zh_CN: field => `${field}必须是11位手机号码`,
+    en: field => `${field}必須是11位手機號碼`,
   },
   validate: value => value.length === 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value),
 });
+
+Vue.use(VeeValidate);
