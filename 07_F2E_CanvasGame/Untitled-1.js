e1cf2a4570dedef3b81518621698a5d098592f28
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2018-07-26 15:35:27 +0800

    demo

---
+++ 07_F2E_CanvasGame/Untitled-1.js
@@ -0,0 +1,15 @@
+data => {
+  // data=你得到的電池 預設值= 0
+  (this.batteries = data), (this.wWidth = this.game.world.width);
+  this.wHeight = this.game.world.height;
+  this.textStyle = {
+    font: '100 20px Helvetica',
+    fill: '#FFF',
+    font: '100 20px Helvetica',
+    font: '100 20px Helvetica',
+    font: '100 20px Helvetica',
+    font: '100 20px Helvetica',
+    font: '100 20px Helvetica',
+    font: '100 20px Helvetica',
+  };
+};
